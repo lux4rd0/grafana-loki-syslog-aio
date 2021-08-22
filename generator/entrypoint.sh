3a89a58dcbf7413e5e6c31d7a05a33f80c5ca554
Dave Schmid <dave@pulpfree.org>
2021-08-21 23:16:22 -0500

    Add files via upload

--- generator/entrypoint.sh
+++ generator/entrypoint.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 n=-1
 c=0
 if [ -n "$3" ]
@@ -26,13 +26,13 @@ random_host=${arr[$rand]}
    I=$(shuf -i 1-4 -n 1)
    D=$(date -u +"%Y-%m-%dT%H:%M:%SZ")
    case "$I" in
-      "1") echo "$D ${random_host} ERROR An error is usually an exception that has been caught and not handled." | nc -4u -w1 ${endpoint} 514
+      "1") echo "$D ${random_host} ERROR An error is usually an exception that has been caught and not handled." | nc -u -w1 ${endpoint} 514
       ;;
-      "2") echo "$D ${random_host} INFO An info is often used to provide context in the current task." | nc -4u -w1 ${endpoint} 514
+      "2") echo "$D ${random_host} INFO An info is often used to provide context in the current task." | nc -u -w1 ${endpoint} 514
       ;;
-      "3") echo "$D ${random_host} WARN A warning that should be ignored is usually at this level and should be actionable." | nc -4u -w1 ${endpoint} 514
+      "3") echo "$D ${random_host} WARN A warning that should be ignored is usually at this level and should be actionable." | nc -u -w1 ${endpoint} 514
       ;;
-      "4") echo "$D ${random_host} DEBUG This is a debug log that shows a log that can be ignored." | nc -4u -w1 ${endpoint} 514
+      "4") echo "$D ${random_host} DEBUG This is a debug log that shows a log that can be ignored." | nc -u -w1 ${endpoint} 514
       ;;
    esac
    c=$(( c+1 ))
